64866f4f5de96ab893bbc4eb8fc376f069d6eefc
Glicio Pereira Uchoa <glicioo@outlook.com>
2023-04-01 17:23:05 -0300

    adjusted size

--- src/components/user/userPFP.tsx
+++ src/components/user/userPFP.tsx
@@ -8,7 +8,7 @@ export default function UserProfilePic() {
 
 
     if(session?.user?.image) return (
-        <div>
+        <div className='w-8 h-8 rounded-full overflow-hidden'>
             <Image src={session.user.image} width={128} height={128} alt="User Profile Picture" />
         </div>
 
