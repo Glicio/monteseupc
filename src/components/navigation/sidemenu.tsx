d3937430186d805966f24d7669b9fc29c093604e
Glicio Pereira Uchoa <glicioo@outlook.com>
2023-04-01 16:40:09 -0300

    created side menu component

---
+++ src/components/navigation/sidemenu.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+export const SideMenuButton = ({
+  label,
+  onClick,
+}: {
+  label: string;
+  onClick?: () => void;
+}) => {
+  return (
+    <button
+      className="w-full px-2 text-left hover:text-[var(--color-contrast)]"
+      onClick={() => {
+        if (onClick) return onClick();
+        return
+      }}
+    >
+      {label}
+    </button>
+  );
+};
+
+export default function SideMenu({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="w-[var(--side-menu-width)] bg-[var(--color-neutral-2)] text-[var(--color-text-primary)]">
+      <div className="fixed flex w-[var(--side-menu-width)] flex-col">
+        {children}
+      </div>
+    </div>
+  );
+}
