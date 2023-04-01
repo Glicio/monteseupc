a5e98dc89ca9c6a4d8c3750034999b2123c3da9f
Glicio Pereira Uchoa <glicioo@outlook.com>
2023-04-01 15:17:09 -0300

    moved navbar to components/navigation

--- src/components/navigation/navbar.tsx
+++ src/components/navigation/navbar.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import UserButton from '../user/userButton'
 
 export default function NavBar() {
     return (
         <header className=' h-[var(--nav-bar-height)] bg-[var(--color-primary)] text-[var(--color-text-primary)] flex items-center px-2 border-b border-[var(--color-neutral-1)]'>
             <h1 className='text-[var(--color-contrast)] font-extrabold text-xl'>Logo</h1>
+            <UserButton/>
         </header>
     )
 }
