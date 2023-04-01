ad7dc2b1432e5f936e6c1f2a69664f016c2ebaa2
Glicio Pereira Uchoa <glicioo@outlook.com>
2023-04-01 17:22:54 -0300

    created a floating menu for the user button

--- src/components/user/userButton.tsx
+++ src/components/user/userButton.tsx
@@ -1,10 +1,40 @@
-import React from 'react'
-import UserProfilePic from './userPFP'
+import React from "react";
+import UserProfilePic from "./userPFP";
+import { signIn, signOut, useSession } from "next-auth/react";
+import UserCircle from "../svg/userCircle";
 
 export default function UserButton() {
-    return (
-        <div>
-            <UserProfilePic/>
+  const { data: session, status } = useSession();
+  const [showMenu, setShowMenu] = React.useState(false);
+
+  return (
+    <div className="relative">
+      <button
+        onClick={() => {
+          setShowMenu((old) => !old);
+        }}
+      >
+        {session?.user ? (
+          <UserProfilePic />
+        ) : (
+          <UserCircle/>
+        )}
+      </button>
+      {showMenu ? (
+        <div className="absolute right-0 top-[100%] whitespace-nowrap rounded-md bg-[var(--color-neutral-1)] p-2">
+          <p>Olá, {session?.user?.name || "Gamer"}!</p>
+          <div className="border-b border-white"></div>
+          {status === "authenticated" ? (
+            <button className="secondary-button" onClick={() => void signOut()}>
+              Sair
+            </button>
+          ) : (
+            <button className="secondary-button" onClick={() => void signIn()}>
+              Entrar
+            </button>
+          )}
         </div>
-    )
+      ) : null}
+    </div>
+  );
 }
