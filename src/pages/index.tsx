524deeab5f722b99b571ff42b2864816191d33e7
Glicio Pereira Uchoa <glicioo@outlook.com>
2023-04-01 15:18:07 -0300

    removed unused code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,13 @@ import { type NextPage } from "next";
 import Head from "next/head";
 import { signIn, signOut, useSession } from "next-auth/react";
 
-import { api } from "../utils/api";
+
 import MainLayout from "../layouts/main";
 
 const Home: NextPage = () => {
+
+
+  const { data: session, status } = useSession();
 
   return (
     <>
@@ -20,6 +23,27 @@ const Home: NextPage = () => {
           <h1 className="text-6xl font-bold text-white">
             Welcome to <a href="https://nextjs.org">MonteSeuPC</a>
           </h1>
+          {status === "loading" && <p>Loading...</p>}
+          {status === "unauthenticated" && (
+            <button
+              className="bg-[var(--color-contrast)] text-[var(--color-primary)] rounded-md p-2"
+              onClick={() => void signIn()}
+            >
+              Sign in
+            </button>
+          )}
+          {status === "authenticated" && (
+            <>
+              <p className="text-white">Signed in as {session?.user?.email}</p>
+              <button
+
+                className="bg-[var(--color-contrast)] text-[var(--color-primary)] rounded-md p-2"
+                onClick={() => void signOut()}
+              >
+                Sign out
+              </button>
+            </>
+          )}
         </div>
       </MainLayout>
     </>
@@ -28,26 +52,4 @@ const Home: NextPage = () => {
 
 export default Home;
 
-const AuthShowcase: React.FC = () => {
-  const { data: sessionData } = useSession();
 
-  const { data: secretMessage } = api.example.getSecretMessage.useQuery(
-    undefined, // no input
-    { enabled: sessionData?.user !== undefined }
-  );
-
-  return (
-    <div className="flex flex-col items-center justify-center gap-4">
-      <p className="text-center text-2xl text-white">
-        {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
-        {secretMessage && <span> - {secretMessage}</span>}
-      </p>
-      <button
-        className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
-        onClick={sessionData ? () => void signOut() : () => void signIn()}
-      >
-        {sessionData ? "Sign out" : "Sign in"}
-      </button>
-    </div>
-  );
-};
