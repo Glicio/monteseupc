d1e43783117493664ed7dd1234fe46b86b294215
Glicio Pereira Uchoa <glicioo@outlook.com>
2023-04-02 11:59:14 -0300

    added ban route and removed dumb try catch

--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -2,7 +2,6 @@ import { z } from "zod";
 import { prisma } from "../../db";
 
 import { createTRPCRouter, adminProcedure } from "../trpc";
-import { TRPCError } from "@trpc/server";
 
 export const user = createTRPCRouter({
   admin: createTRPCRouter({
@@ -14,73 +13,86 @@ export const user = createTRPCRouter({
           skip: z.number().optional(),
         })
       )
-      .mutation(async ({ input, ctx }) => {
+      .mutation(async ({ input }) => {
         const { searchTerm } = input;
-        try {
-          const count = await prisma.user.count({
-            where: searchTerm
-              ? {
-
-                  OR: [
-                    {
-                      name: {
-                        contains: searchTerm,
-                      },
+        const count = await prisma.user.count({
+          where: searchTerm
+            ? {
+                OR: [
+                  {
+                    name: {
+                      contains: searchTerm,
                     },
-                    {
-                      email: {
-                        contains: searchTerm,
-                      },
+                  },
+                  {
+                    email: {
+                      contains: searchTerm,
                     },
-                  ],
-                }
-              : undefined,
-          });
-          const users = await prisma.user.findMany({
-            where: searchTerm
-              ? {
-                  OR: [
-                    {
-                      name: {
-                        contains: searchTerm,
-                      },
+                  },
+                ],
+              }
+            : undefined,
+        });
+        const users = await prisma.user.findMany({
+          where: searchTerm
+            ? {
+                OR: [
+                  {
+                    name: {
+                      contains: searchTerm,
                     },
-                    {
-                      email: {
-                        contains: searchTerm,
-                      },
+                  },
+                  {
+                    email: {
+                      contains: searchTerm,
                     },
-                  ],
-                }
-              : undefined,
-            take: input.take || 10,
-            skip: input.skip || 0,
-          });
-          return {users, count}
-        } catch (error) {
-          throw new TRPCError({code: "INTERNAL_SERVER_ERROR", message: "Erro interno do servidor"});
-        }
+                  },
+                ],
+              }
+            : undefined,
+          take: input.take || 10,
+          skip: input.skip || 0,
+        });
+        return { users, count };
       }),
-      setRole: adminProcedure.input(z.object({
-        id: z.string(),
-        role: z.enum(['ADMIN', 'USER', 'MODERATOR'])
-      })).mutation(async ({input}) => {
-        const {id} = input
-        try {
-          const user = await prisma.user.update({
-            where: {
-              id
-            },
-            data: {
-              isAdmin: input.role === 'ADMIN',
-              isMod: input.role === 'MODERATOR',
-            }
-          })
-          return user
-        } catch (error) {
-          throw new TRPCError({code: "INTERNAL_SERVER_ERROR", message: "Erro interno do servidor"});
-        }
-      }
-      ),
+    setRole: adminProcedure
+      .input(
+        z.object({
+          id: z.string(),
+          role: z.enum(["ADMIN", "USER", "MODERATOR"]),
+        })
+      )
+      .mutation(async ({ input }) => {
+        const { id } = input;
+        const user = await prisma.user.update({
+          where: {
+            id,
+          },
+          data: {
+            isAdmin: input.role === "ADMIN",
+            isMod: input.role === "MODERATOR",
+          },
+        });
+        return user;
+      }),
+    banUser: adminProcedure
+      .input(
+        z.object({
+          id: z.string(),
+          ban: z.boolean(),
+        })
+      )
+      .mutation(async ({ input }) => {
+        const { id } = input;
+        const user = await prisma.user.update({
+          where: {
+            id,
+          },
+          data: {
+            isBanned: input.ban,
+          },
+        });
+        return user;
+      }),
   }),
 });
