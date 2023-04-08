95f39a6636be2c0b911b785fb1e714b898dc7814
Glicio Pereira Uchoa <glicioo@outlook.com>
2023-04-08 16:14:23 -0300

    number input now works as intended

--- src/components/input/defaultNumberInput.tsx
+++ src/components/input/defaultNumberInput.tsx
@@ -5,7 +5,7 @@ export default function DefaultNumberInput({
   setValue,
   title,
   placeholder,
-  required
+  required,
 }: {
   value: number;
   setValue: (value: number) => void;
@@ -13,16 +13,35 @@ export default function DefaultNumberInput({
   placeholder?: string;
   required?: boolean;
 }) {
+  const [valueState, setValueState] = React.useState<number | string>(value);
+
   return (
-    <div className="form-item flex flex-col w-full">
-      <label className="form-label">{title}</label>
+    <div className="form-item flex w-full flex-col">
+      <label className="form-label text-sm font-bold">{title}</label>
       <input
         className="default-text-input"
         type="number"
         required={required || false}
-        value={value}
+        pattern="[0-9]*"
+        value={valueState}
+        onKeyDown={(event) => {
+          if (
+            !/[0-9]/.test(event.key) &&
+            event.key !== "Backspace" &&
+            event.key !== "ArrowLeft" &&
+            event.key !== "ArrowRight" &&
+            event.key !== "ArrowUp" &&
+            event.key !== "ArrowDown" &&
+            event.key !== "Delete"
+          ) {
+            event.preventDefault();
+          }
+        }}
         onChange={(e) => {
-          setValue(Math.ceil(Number(e.target.value)));
+          const value = e.target.value.replace(/^0+/g, "");
+
+          setValueState(value);
+          setValue(Math.ceil(Number(value)));
         }}
         placeholder={placeholder || ""}
       />
