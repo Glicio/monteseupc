3f267ce9fdcb34ac83330a12dc920b941ddb58d6
Glicio Pereira Uchoa <glicioo@outlook.com>
2023-04-10 19:26:00 -0300

    fixed a issue where ddr4 and ddr5 had the same value

--- src/components/input/selectRam.tsx
+++ src/components/input/selectRam.tsx
@@ -5,7 +5,7 @@ export default function SelectRamType({value, setValue}: {value: string, setValu
         <div className='flex flex-col whitespace-nowrap'>
             <label htmlFor="ramType">Tipo de Memória</label>
             <select name="ramType" id="ramType" className='default-select-input' value={value} onChange={(e) => setValue(e.target.value)}>
-                <option value="DDR4">DDR5</option>
+                <option value="DDR5">DDR5</option>
                 <option value="DDR4">DDR4</option>
                 <option value="DDR3">DDR3</option>
                 <option value="DDR2">DDR2</option>
