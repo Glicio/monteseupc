8938ca8219d8b1f84d6420116333d83587028491
Glicio Pereira Uchoa <glicioo@outlook.com>
2023-04-14 21:16:57 -0300

    installed the package database js from @planetscale and added a connection to db.js (the same file of the prisma instance)

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,19 +1,30 @@
+import { connect } from "@planetscale/database";
 import { PrismaClient } from "@prisma/client";
 
 import { env } from "../env/server.mjs";
 
 declare global {
-  // eslint-disable-next-line no-var
-  var prisma: PrismaClient | undefined;
+    // eslint-disable-next-line no-var
+    var prisma: PrismaClient | undefined;
 }
 
+const config = {
+    host: process.env.DATABASE_HOST,
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+};
+
+export const db = connect(config);
+
 export const prisma =
-  global.prisma ||
-  new PrismaClient({
-    log:
-      env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
-  });
+    global.prisma ||
+    new PrismaClient({
+        log:
+            env.NODE_ENV === "development"
+                ? ["query", "error", "warn"]
+                : ["error"],
+    });
 
 if (env.NODE_ENV !== "production") {
-  global.prisma = prisma;
+    global.prisma = prisma;
 }
