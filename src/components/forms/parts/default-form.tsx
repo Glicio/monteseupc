e0089b0d7380ecc3dca9b52869e133991a6d16d8
Glicio Pereira Uchoa <glicioo@outlook.com>
2023-04-16 16:02:08 -0300

    created a form component

--- src/components/forms/parts/default-form.tsx
+++ src/components/forms/parts/default-form.tsx
@@ -1,20 +1,28 @@
 import React from "react";
 
-
 interface DefaultFormProps {
     title: string;
     onSubmit: () => void;
     children: React.ReactNode;
 }
 
-
-export default function DefaultForm({title, onSubmit, children}: DefaultFormProps) {
+export default function DefaultForm({
+    title,
+    onSubmit,
+    children,
+}: DefaultFormProps) {
     return (
-        <div className="flex flex-col w-full">
-            <h2 className="text-2xl font-bold">Informações gerais</h2>
-            <form action="" onSubmit={(e) => {e.preventDefault()}}>
-            {children}
+        <div className="flex w-full flex-col">
+            <h2 className="text-2xl font-bold">{title}</h2>
+            <form
+                action=""
+                onSubmit={(e) => {
+                    e.preventDefault();
+                    onSubmit();
+                }}
+            >
+                {children}
             </form>
         </div>
-);
+    );
 }
